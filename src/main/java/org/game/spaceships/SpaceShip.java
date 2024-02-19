060c17b3c3369c7336c4d30700f18fba9f7deb58
Kacper Jarocki <kacper.jarocki1121@gmail.com>
2024-02-19 12:52:19 +0100

    interface implamentation and basic unit testing

--- src/main/java/org/game/spaceships/SpaceShip.java
+++ src/main/java/org/game/spaceships/SpaceShip.java
@@ -1,10 +1,40 @@
 package org.game.spaceships;
 
-public class SpaceShip {
+import org.game.interfaces.Drawable;
+import org.game.interfaces.Movable;
+
+public class SpaceShip implements Drawable, Movable {
 	int x;
 	int y;
+	String image;
 	public SpaceShip(int x,int y){
 		this.x=x;
 		this.y=y;
+		this.image = "A";
+	}
+
+	@Override
+	public int positionX() {
+		return x;
+	}
+
+	@Override
+	public int positionY() {
+		return y;
+	}
+
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	@Override
+	public void moveRight() {
+		x++;
+	}
+
+	@Override
+	public void moveLeft() {
+		x--;
 	}
 }
--- src/test/java/org/game/spaceships/SpaceShipTest.java
+++ src/test/java/org/game/spaceships/SpaceShipTest.java
@@ -1,4 +1,40 @@
+package org.game.spaceships;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SpaceShipTest {
-  
+	SpaceShip spaceShip;
+	@BeforeEach
+	void setUp() {
+		spaceShip = new SpaceShip(10, 0);
+	}
+	@Test
+	void positionX() {
+		assertEquals(10, spaceShip.positionX());
+	}
+
+	@Test
+	void positionY() {
+		assertEquals(0, spaceShip.positionY());
+	}
+
+	@Test
+	void getImage() {
+		assertEquals("A", spaceShip.getImage());
+	}
+
+	@Test
+	void moveRight() {
+		spaceShip.moveRight();
+		assertEquals(11, spaceShip.positionX());
+	}
+
+	@Test
+	void moveLeft() {
+		spaceShip.moveLeft();
+		assertEquals(9, spaceShip.positionX());
+	}
 }
